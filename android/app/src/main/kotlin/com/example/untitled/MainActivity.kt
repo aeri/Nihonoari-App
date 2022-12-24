ae59849d074c93400adf038902ed41af99029480
NAVAL <navaldesu@yahoo.co.jp>
2022-12-24 18:51:40 +0100

    demotion: AGP tools to 7.2.1
    Trying to fix run in API < 20

--- android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
@@ -1,6 +1,23 @@
-package com.example.untitled
+package com.LAPARCELA.nihonoari
 
 import io.flutter.embedding.android.FlutterActivity
+import android.content.Intent.getIntent
+
+import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        /*
+    if (Build.VERSION.SDK_INT < 20) {
+      intent.putExtra("enable-software-rendering", true)
+    }
+         */
+            getIntent().putExtra("enable-software-rendering", true);
+
+        // start Flutter
+        super.onCreate(savedInstanceState)
+    }
+
 }
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Ariel\Projects\Nihonoari-App"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.7.1"
+export "FLUTTER_BUILD_NUMBER=11"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
