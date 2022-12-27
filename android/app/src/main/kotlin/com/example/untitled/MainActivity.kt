670257046922aad3500e31dd3e410c12f01e9b09
NAVAL <navaldesu@yahoo.co.jp>
2022-12-27 02:21:16 +0100

    make: AGP version changes and splash screen

--- android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
@@ -1,6 +1,19 @@
-package com.example.untitled
+package com.LAPARCELA.nihonoari
 
 import io.flutter.embedding.android.FlutterActivity
+import android.os.Build
+import android.content.Intent.getIntent
+
+import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT < 20) {
+            getIntent().putExtra("enable-software-rendering", true);
+        }
+        // start Flutter
+        super.onCreate(savedInstanceState)
+    }
+
 }
