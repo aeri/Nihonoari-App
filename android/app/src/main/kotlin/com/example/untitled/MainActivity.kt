0089024ffa97e3944cb3636f3952b814accf66f4
aeri <ari@naval.cat>
2026-09-23 16:53:47 +0200

    fix: answer space trimming
    changed deprecated theme options

--- android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/untitled/MainActivity.kt
@@ -6,19 +6,4 @@ import android.content.Intent.getIntent
 import android.os.Bundle
 
 class MainActivity: FlutterActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-
-        /*
-    if (Build.VERSION.SDK_INT < 20) {
-      intent.putExtra("enable-software-rendering", true)
-      getIntent().putExtra("enable-software-rendering", true);
-
-    }
-         */
-
-        // start Flutter
-        super.onCreate(savedInstanceState)
-    }
-
 }
